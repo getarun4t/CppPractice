11e4eed2225c8f3e1c22f82e97f100b5da7bdbe1
Arun Sasi <aruntdpa@gmail.com>
2023-06-25 18:08:31 +0200

    Adding code

---
+++ Path Check/Path Check/Path Finder.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+pair<int, int> findSource(vector<vector<string>> field, int b, int l) {
+	int source = 0;
+	for (int i = 0; i < l; i++) {
+		for (int j = 0; j < b; j++) {
+			if (field[i][j] == "A") {
+				pair<int, int> loc;
+				loc.first = i;
+				loc.second = j;
+				return loc;
+			}
+		}
+	}
+}
+
+bool checkBreadth(vector<vector<string>> field, int b, int l, int x, int y) {
+	for (int i = y; i < b; i++) {
+		if (field[x][i] == "x" or field[x][i] == ">" or field[x][i] == "v")
+			break;
+		else if (field[x][i] == "<")
+			return false;
+	}
+	for (int i = y-1; i >= 0;  i--) {
+		if (field[x][i] == "x" or field[x][i] == "<" or field[x][i] == "v")
+			return true;
+		else if (field[x][i] == ">")
+			return false;
+	}
+	return true;
+}
+
+bool checkLength(vector<vector<string>> field, int b, int l, int x, int y) {
+	for (int i = x; i >=0; i--) {
+		if (field[i][y] == "x" or field[i][y] == ">" or field[i][y] == "<")
+			return true;
+		else if (field[i][y] == "v")
+			return false;
+	}
+	return true;
+}
+
+bool pathHelper(vector<vector<string>> field, int b, int l, int init_a, int init_b) {
+	cout << "Current position is (" << init_a << " ," << init_b << ")" << endl;
+	if (init_a == l - 1 && init_b == b - 1) {
+		cout << "Reached destination : (" << init_a << " ," << init_b << ")" << endl;
+		return true;
+	}
+	if (init_a-1 >=0){
+		if (field[init_a - 1][init_b] == "." &&
+			checkBreadth(field, b, l, init_a - 1, init_b)) {
+			field[init_a - 1][init_b] = "A";
+			if (pathHelper(field, b, l, init_a - 1, init_b)) {
+				return true;
+			};
+		}
+	}
+	if(init_b - 1 >= 0) {
+		if (field[init_a][init_b - 1] == "." &&
+			checkLength(field, b, l, init_a, init_b - 1)) {
+			field[init_a][init_b - 1] = "A";
+			if (pathHelper(field, b, l, init_a, init_b - 1)) {
+				return true;
+			};
+		}
+	}
+	if (init_b + 1 < b) {
+		if (field[init_a][init_b + 1] == "." &&
+			checkLength(field, b, l, init_a, init_b + 1)) {
+			field[init_a][init_b + 1] = "A";
+			if (pathHelper(field, b, l, init_a, init_b + 1)) {
+				return true;
+			};
+		}
+	}
+	if (init_a + 1 < l){
+		if (field[init_a + 1][init_b] == "." &&
+			checkBreadth(field, b, l, init_a + 1, init_b)) {
+			field[init_a + 1][init_b] = "A";
+			if (pathHelper(field, b, l, init_a + 1, init_b)) {
+				return true;
+			};
+		}
+	}
+	if (field[init_a][init_b] == "A") {
+		cout << "Going back (" << init_a << " ," << init_b << ")" << endl;
+		return false;
+	}
+	return true;
+}
+
+bool pathCheck(vector<vector<string>> field) {
+	bool pos_flag;
+	const int b = field.size();
+	const int l = field[0].size();
+	pair<int, int> Node = findSource(field, b, l);
+	int init_a = Node.first;
+	int init_b = Node.second;
+	return pathHelper(field, l, b, init_a, init_b);
+}
+
+int main() {
+	vector<vector<string>> field =
+	{	{"x", ".", "." ,".", "<"},
+		{".", ".", ".", ".", "x"},
+		{".", ">", "x", ".", "."},
+		{"A", ".", ".", "x", "."} };
+
+	cout << pathCheck(field) << endl;
+}
