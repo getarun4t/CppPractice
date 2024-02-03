13eb3552bb24d578103507c9cd914958ac76809e
Arun Sasi <aruntdpa@gmail.com>
2024-02-03 19:48:46 +0100

    Add all combin

--- LambdaDemo/LambdaDemo.cpp
+++ LambdaDemo/LambdaDemo.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 int main() {
-	int i;
-
-	auto a = [](int arg) {return 2 * arg; };
+	auto a = [](int arg) {return 2 * arg;};
 	std::cout << "First free = " << a(4) << std::endl;
 
+	int i = 3;
+	auto b = [i](int arg) {return i * arg; };
+	std::cout << "Second free = " << b(4) << std::endl;
 
+	int j = 4;
+	auto c = [=](int arg) {return j*arg; };
+	std::cout << "Third free = " << c(4) << std::endl;
+
+	int k = 4;
+	auto d = [&]() {return k++; };
+	std::cout << "Fourth free = " << d() << std::endl;
+	std::cout << "Ref change = " << k << std::endl;
 }
