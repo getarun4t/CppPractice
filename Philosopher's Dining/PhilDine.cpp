b2f8f91478135530d29deec230841da68c7e956f
Arun Sasi <38099892+getarun4t@users.noreply.github.com>
2024-02-14 21:00:52 +0100

    Adding a basic logic (#77)

--- Philosopher's Dining/PhilDine.cpp
+++ Philosopher's Dining/PhilDine.cpp
@@ -1,6 +1,7 @@
-#include <mutex>
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <shared_mutex>
 
 std::thread t1;
 std::thread t2;
@@ -8,7 +9,7 @@ std::thread t2;
 class Philosopher {
 	bool eat;
 	bool think;
-
+public:
 	Philosopher() {
 		eat = false;
 		think = true;
@@ -23,8 +24,41 @@ class Philosopher {
 		eat = false;
 		think = true;
 	}
+
+	bool get_status() {
+		if (eat) {
+			return true;
+		}
+		return false;
+	}
+};
+
+struct Table {
+	std::vector<Philosopher> phils;
+	std::vector<std::shared_mutex> fork;
+	Table(int n) : phils(n), fork(n) {};
+
+	void get_table_status() {
+		int philc = 0;
+		for (auto& p : phils) {
+			if (p.get_status()) {
+				std::cout << "Philosoper " << philc << " is eating" << std::endl;
+			}
+			else {
+				std::cout << "Philosoper " << philc << " is thinking" << std::endl;
+			}
+		}
+	}
+
+	void serve_food() {
+		for (size_t i = 0; i < fork.size(); ++i) {
+			std::unique_lock<std::shared_mutex> lock(fork[i]);
+			std::cout << "Serving food to Philosopher " << i << std::endl;
+		}
+	}
 };
 
 int main() {
-
+	Table table(5);
+	table.serve_food();
 }
